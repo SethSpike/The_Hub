52538106217ee13a6280e702957bf98601594c91
Seth <78033226+SethSpike@users.noreply.github.com>
2022-12-01 19:39:17 -0500

    Fixed Teleport

---
+++ Assets/The_Hub/Scripts/Load_Scene_Hub.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Scene_Hub : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("The_Hub_V1"); //the index of the seconf scene
+    }
+
+}
